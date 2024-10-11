661d200ab969f5016b55c877c39124d3d97d88e9
tbysyl <toby.sayle@gmail.com>
2024-10-11 17:57:50 +0100

    Pygame testing

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 
-print("Hello World!")
+import pygame as pg
+
+pg.init()
+
+screen = pg.display.set_mode([500,500])
+
+running = True
+while running:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    screen.fill((255,255,255))
+
+    pg.draw.circle(screen,(0,0,255),(250,250),75)
+
+    pg.display.flip()
+
+pg.quit()
+                    
