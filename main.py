32856f854228434822f55eedd6cd92d288cb23fb
tbysyl <toby.sayle@gmail.com>
2024-10-11 19:15:58 +0100

    More pygame testing

--- main.py
+++ main.py
@@ -3,20 +3,39 @@ import pygame as pg
 
 pg.init()
 
-screen = pg.display.set_mode([500,500])
+width,height = 800,600
+screen = pg.display.set_mode((width,height))
+pg.display.set_caption("Boid Testing")
 
-running = True
-while running:
+circle_x = width // 2
+circle_y = height // 2
+circle_radius = 25
+circle_speed = 5
+
+while True:
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            running = False
+            pg.quit()
 
-    screen.fill((255,255,255))
-
+    screen.fill((0,0,0))
     pg.draw.circle(screen,(0,0,255),(250,250),75)
 
+    keys = pg.key.get_pressed()
+    
+    # Move the circleangle
+    if keys[pg.K_LEFT]:
+        circle_x -= circle_speed
+    if keys[pg.K_RIGHT]:
+        circle_x += circle_speed
+    if keys[pg.K_UP]:
+        circle_y -= circle_speed
+    if keys[pg.K_DOWN]:
+        circle_y += circle_speed
+
     pg.display.flip()
+
+    pg.time.Clock().tick(60)
 
 pg.quit()
                     
